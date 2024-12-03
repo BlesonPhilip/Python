13f8ad4d101d97eeb24df316491fb5905b90f2b9
BlesonPhilip <blesonphilip21@gmail.com>
2024-12-03 09:41:25 +0530

    Swap 2 no.s

---
+++ swap2no.py
@@ -0,0 +1,9 @@
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+print("before swapping first no",num1)
+print("before swapping second no",num2)
+temp=num1
+num1=num2
+num2=temp
+print("after swapping first number",num1)
+print("after swapping second number",num2)
